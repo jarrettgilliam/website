34ef67a1e2cd5348d825e1c5375afd65396e96fd
Jarrett Gilliam <jarrettgilliam@gmail.com>
2024-10-16 18:29:44 -0500

    Fix the resume button on safari

--- src/Website/wwwroot/script.js
+++ src/Website/wwwroot/script.js
@@ -9,15 +9,18 @@ document.addEventListener('DOMContentLoaded', function () {
 document.getElementById("btn-getResume").addEventListener("click", getResume);
 document.getElementById("btn-getEmail").addEventListener("click", getEmail);
 
-function getResource(baseUri, callback) {
+function getResource(baseUri, onSuccess, onError) {
     grecaptcha.ready(function () {
         grecaptcha.execute('6LdQ4uIgAAAAAAXQbFEfBlACvn8lRh6txqQhcy_6', {action: 'submit'}).then(async function (token) {
-            let response = await fetch(`${baseUri}?token=${token}`);
+            const response = await fetch(`${baseUri}?token=${token}`);
 
             if (response.ok) {
-                let resourceLink = await response.text();
-                callback(resourceLink);
+                const resourceLink = await response.text();
+                onSuccess(resourceLink);
             } else {
+                if (onError) {
+                    onError();
+                }
                 alert("HTTP-Error: " + response.status);
             }
         });
@@ -31,7 +34,8 @@ function getEmail() {
 }
 
 function getResume() {
-    getResource('api/Resource/GetResume', r => {
-        window.open(r);
-    });
+    const w = window.open();
+    getResource('api/Resource/GetResume',
+            r => { w.location = r; },
+            () => { w.close(); });
 }
